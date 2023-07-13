a9e1cf9d3ea7ef31b8de5260d27c6af5bf8c6998
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-07-13 01:55:06 -0700

    frontend ready for melding

--- src/components/sideLeft/SideLeft.jsx
+++ src/components/sideLeft/SideLeft.jsx
@@ -1,11 +1,11 @@
 import "./sideLeft.css"
 import Calendar from "react-calendar"
 
-export default function SideLeft() {
-  return (
-    <div className="sideLeftShroud">
-      <div className="sideLeftCloak">
-        <div className="sideLeftAbove">
+export default function SideLeft({ dashboard }) {
+  const HomeSideLeft = () => {
+    return (
+      <>
+      <div className="sideLeftAbove">
           <div className="sideLeftCalendar">
             <Calendar />
           </div>
@@ -18,6 +18,42 @@ export default function SideLeft() {
             ))}
           </ul>
         </div>
+      </>
+    )
+  };
+
+  const DashboardSideLeft = () => {
+    return (
+      <>
+      <h3 className="sideLeftTitle">My deets</h3>
+      <div className="sideLeftDeets">
+        <div className="sideLeftDeet">
+          <span className="sideLeftDeetKey">From:</span>
+          <span className="sideLeftDeetValue">Colorado</span>
+        </div>
+        <div className="sideLeftDeet">
+          <span className="sideLeftDeetKey">City:</span>
+          <span className="sideLeftDeetValue">Portland</span>
+        </div>
+        <div className="sideLeftDeet">
+          <span className="sideLeftDeetKey">Dream:</span>
+          <span className="sideLeftDeetValue">County Cork</span>
+        </div>
+      </div>
+      <h3 className="sideLeftTitle">My peeps</h3>
+      <div className="sideLeftPeeps">
+        <div className="sideLeftPeep">
+          <img src={} alt="" className="sideLeftPeepImg" />
+          <span className="sideLeftPeepName">John Doe</span>
+        </div>
+        </div>
+      </>
+    )
+  }
+  return (
+    <div className="sideLeftShroud">
+      <div className="sideLeftCloak">
+        {profile ? <DashboardSideLeft /> : <HomeSideLeft />}
       </div>
     </div>
   )
